be1d2e5e875dbfbb44372637ad3fd98004372edc
Jeremy Sparagon <j.a.sparagon@outlook.com>
2023-07-03 00:15:04 -0700

    set up basic structure with Parcel and React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as ReactDOM from "react-dom/client";
+
+export function App() {
+
+
+
+  return (
+    <main>
+
+	    <h1>Deep Chalk</h1>
+	    <h3>By Zack Livestone. Archived from <a href="https://jayisgames.com">Jayisgames</a> using <a href="https://ruffle.rs">Ruffle</a> for Flash emulation.</h3>
+      
+
+    </main>
+  );
+}
