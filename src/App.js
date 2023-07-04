4f34ec582755858950abeefbf8110b0a16cf9054
Jeremy Sparagon <j.a.sparagon@outlook.com>
2023-07-04 14:03:23 -0700

    phase selector

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import * as ReactDOM from "react-dom/client";
+import '@ruffle-rs/ruffle/ruffle.js';
 
 export function App() {
 
-
+    const phases = [
+        {id: 1, name: "First Phase"},
+        {id: 2, name: "Second Phase"},
+        {id: 3, name: "Third Phase"},
+        {id: 4, name: "Deep Chalk.4"}
+    ];
+    
 
   return (
     <main>
@@ -11,6 +18,16 @@ export function App() {
 	    <h1>Deep Chalk</h1>
 	    <h3>By Zack Livestone. Archived from <a href="https://jayisgames.com">Jayisgames</a> using <a href="https://ruffle.rs">Ruffle</a> for Flash emulation.</h3>
       
+
+	<label for="phase">Select a Deep Chalk Phase:
+	    <br />
+	    <select name="phase" id="phase">
+		{phases.map(phase =>
+		    <option name={phase.id}>{phase.name}</option>
+		)}
+	        </select>
+        </label>
+
 
     </main>
   );
